7af83a8ee2839ffd7553e6706aa35028caf87bd9
josiedlee <josie.lee@menloschool.org>
2023-11-28 13:45:15 -0800

    Finished Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    private char rank;
+    private String suit;
+    private int point;
+
+    public Card(char newRank, String newSuit, int newPoint) {
+        rank = newRank;
+        suit = newSuit;
+        point = newPoint;
+    }
+
+    public char getRank() {
+        return rank;
+    }
+
+    public void setRank(char rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
