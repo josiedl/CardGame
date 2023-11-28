251cb55231d15ecaedf430fa2b88fde887653963
josiedlee <josie.lee@menloschool.org>
2023-11-28 13:45:40 -0800

    Started Deck Class

---
+++ src/Deck.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(char[] rank, ) {
+        cards = new ArrayList<Card>();
+    }
+}
