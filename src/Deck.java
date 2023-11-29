908001ab80aff4b2497cbf85692dde75d54ea522
josiedlee <josie.lee@menloschool.org>
2023-11-29 13:41:19 -0800

    Worked on Deck Class

--- src/Deck.java
+++ src/Deck.java
@@ -4,7 +4,25 @@ public class Deck {
     private ArrayList<Card> cards;
     private int cardsLeft;
 
-    public Deck(char[] rank, ) {
+    public Deck(char[] rank, String[]suit, int[]point) {
         cards = new ArrayList<Card>();
+        // DON'T KNOW HOW TO YET
+        for (int i = 0; i < 5; i++) {
+            Card newCard = new Card(rank[i], suit[i], point[i]);
+            cards.add(newCard);
+        }
+    }
+
+    public boolean isEmpty() {
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+
+    public Card deal() {
+        // LEFT OFF HERE
+        return null;
     }
 }
