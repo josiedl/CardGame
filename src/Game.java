69b1402af88a1c98459356d2f56a500d0cf47350
josiedlee <josie.lee@menloschool.org>
2023-12-06 22:45:55 -0800

    Worked on Game Class

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Game {
-    public static void main(String[] args) {
-        System.out.print(Deck({'A', '2', '3'}, {"Hearts", "Clubs"}, {1,2,3}));
+    // Instance Variables
+    private Player player1;
+    private Player player2;
+    private Deck deck;
+    Scanner input = new Scanner(System.in);
+    String name1 = input.nextLine();
+    System.out.println("Player 1 enter a name: ");
+    String name2 = input.nextLine();
+    System.out.println("Player 2 enter a name: ");
+    // Constructor
+    public Game() {
+        char ranks[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',};
+        String suits[] = {"Yellow Tang", "Angelfish", "Purple Tang",
+                            "Clownfish", "Angler Fish", "Swordfish", "Whale",
+                                "Shark", "Seahorse", "Dolphin", "Jellyfish",
+                                    "Flounder", "Octopus"};
+        int points[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        deck = new Deck(ranks[], suits[], points[]);
+        player1 = new Player(name1);
+        player2 = new Player(name2);
     }
+    // MAIN
+    // public static void main(String[] args) {
+    //
+    // }
 }
